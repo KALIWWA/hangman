83f0e763f7ad09a5ac2c585a3edec9a9754b62d9
Michal Kalinowski <subaru.kali@gmail.com>
2019-02-19 20:39:08 +0100

    add favicon

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def route_index():
     return render_template("hangman.html")
 
 
 @app.route('/pl')
-def polish():
+def route_polish():
     return render_template("hangman_pl.html")
+
+
+@app.route('/favicon.ico')
+def route_favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'dead.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == '__main__':
